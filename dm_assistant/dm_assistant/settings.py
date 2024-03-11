0a97c8a48fae199873a5db2ce1f4263c0db5d616
i3link <131900764+i3link@users.noreply.github.com>
2024-03-11 09:47:34 -0700

    Update settings.py

--- dm_assistant/dm_assistant/settings.py
+++ dm_assistant/dm_assistant/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['dmassistant-production.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['dmassistant-production.up.railway.app', '127.0.0.1', 'www.rpgsage.ai', 'rpgsage.ai']
 CSRF_TRUSTED_ORIGINS = ['https://dmassistant-production.up.railway.app']
 
 
